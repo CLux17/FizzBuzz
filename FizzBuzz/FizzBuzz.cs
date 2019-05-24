b9b93684444b978d9a6ecad365d7c28a267a6fc8
CLux17 <cplus7@gmail.com>
2019-05-23 20:54:57 -0400

    first commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string Get(int num)
+        {
+            return null;
+        }
+    }
+
+
+}
