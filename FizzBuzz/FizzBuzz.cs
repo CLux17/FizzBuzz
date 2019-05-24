67cf325adf720fd04336b7557d2c99b54ec48903
CLux17 <cplus7@gmail.com>
2019-05-23 21:43:31 -0400

    complete fizzbuzz assignment

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -8,7 +8,28 @@ namespace FizzBuzz
     {
         public string Get(int num)
         {
-            return null;
+            if (IsMultipleOf3(num) && IsMultipleOf5(num))
+            {
+                return "FizzBuzz";
+            }
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+                return num.ToString();
+        }
+
+        private bool IsMultipleOf3(int numberToCHeck)
+        {
+            return numberToCHeck % 3 == 0;
+        }
+        private bool IsMultipleOf5(int numberToCHeck)
+        {
+            return numberToCHeck % 5 == 0;
         }
     }
 }
